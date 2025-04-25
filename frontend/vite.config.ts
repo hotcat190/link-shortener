c61991e9140c3f2727ccc9f8abe84c500a7bc260
LuongManhLinh <luongmanhlinh2004cb@gmail.com>
2025-04-25 15:46:15 +0700

    Update Branch For Public IP

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,33 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import os from "os";
-
-// Helper to get local IP address
-function getLocalExternalIP() {
-  const interfaces = os.networkInterfaces();
-  for (const name of Object.keys(interfaces)) {
-    for (const iface of interfaces[name]) {
-      if (iface.family === "IPv4" && !iface.internal) {
-        return iface.address;
-      }
-    }
-  }
-  return "localhost";
-}
-
-const localIP = getLocalExternalIP();
 
 export default defineConfig({
   plugins: [react()],
-  define: {
-    __LOCAL_IP__: JSON.stringify(localIP),
-  },
   server: {
     host: "0.0.0.0",
     port: 5173,
     proxy: {
       "/api": {
-        target: `http://localhost`, // your backend
+        target: `http://13.229.127.85`, // your backend
         changeOrigin: true,
       },
     },
