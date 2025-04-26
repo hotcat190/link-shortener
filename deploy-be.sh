f7ea523270378dff51d955a54b40a0a553af33bf
LuongManhLinh <luongmanhlinh2004cb@gmail.com>
2025-04-26 17:26:27 +0700

    Init branch

---
+++ deploy-be.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# This script is used to install and run backend only on Ubuntu 24.04
+# Used in case you want to separate backend and frontend deployment
+# If you want to run both backend and frontend, please use deploy.sh in branch cloud-deploy
+
+# NOTE: Before running this script, make sure you have the following:
+# 1. Install Git
+# 2. Clone this repository
+# 3. Switch to the cloud-deploy branch
+# 4. Make this script executable: chmod +x deploy-be.sh
+# 5. Run this script: ./deploy-be.sh
+
+# Remove old versions of docker
+for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
+
+# Install dependencies
+# Add Docker's official GPG key:
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+
+# Install Docker Engine, containerd, and Docker Compose
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+
+# Start the services
+# Make sure to run this script from the root of the project
+set -e
+
+
+echo "🚀 Building and starting backend..."
+cd backend
+sudo docker compose up --build
