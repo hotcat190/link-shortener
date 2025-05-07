82d96b9a048c2b89eec3585bb0469f52d7a1d8b8
LuongManhLinh <luongmanhlinh2004cb@gmail.com>
2025-05-07 22:48:55 +0700

    Change IP Addresses

--- frontend/src/constants.ts
+++ frontend/src/constants.ts
@@ -1,2 +1,2 @@
-export const BASE_BACKEND_URL = "http://52.77.222.62:8080/api";
-export const BASE_FRONTEND_URL = `http://52.77.235.14`;
+export const BASE_BACKEND_URL = "http://18.141.196.24/api";
+export const BASE_FRONTEND_URL = `http://13.215.249.189`;
