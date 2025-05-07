7e063d40ada61a81bd32faf475e2b15446aeb952
LuongManhLinh <luongmanhlinh2004cb@gmail.com>
2025-05-07 23:05:36 +0700

    Update constants.ts

--- frontend/src/constants.ts
+++ frontend/src/constants.ts
@@ -1,2 +1,2 @@
-export const BASE_BACKEND_URL = "http://18.141.196.24/api";
+export const BASE_BACKEND_URL = "http://18.141.196.24:8080/api";
 export const BASE_FRONTEND_URL = `http://13.215.249.189`;
