a11f3941dff4af48ca837aa956f8ebdded0a85d2
LuongManhLinh <luongmanhlinh2004cb@gmail.com>
2025-05-09 14:53:37 +0700

    Update

--- frontend/src/constants.ts
+++ frontend/src/constants.ts
@@ -1,2 +1,2 @@
-export const BASE_BACKEND_URL = process.env.BASE_BACKEND_URL 
-export const BASE_FRONTEND_URL = process.env.BASE_FRONTEND_URL 
+export const BASE_BACKEND_URL = import.meta.env.VITE_BASE_BACKEND_URL
+export const BASE_FRONTEND_URL = import.meta.env.VITE_BASE_FRONTEND_URL
